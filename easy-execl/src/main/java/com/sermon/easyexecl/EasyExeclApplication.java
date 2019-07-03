135606b4c2bb66887f5fce47264f155c917c3e77
xlkarstens <867184382@qq.com>
2019-07-03 17:09:47 +0800

    布道师系列首次提交，目前需要完善两个项目 幂等操作和easy-excel的使用。

---
+++ easy-execl/src/main/java/com/sermon/easyexecl/EasyExeclApplication.java
@@ -0,0 +1,13 @@
+package com.sermon.easyexecl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyExeclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyExeclApplication.class, args);
+    }
+
+}
