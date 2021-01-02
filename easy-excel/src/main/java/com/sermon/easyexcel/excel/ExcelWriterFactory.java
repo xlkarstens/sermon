f7ef65d460ca12e1fbf89b29ea3c72fd0a820d48
huanglongquan <867184382@qq.com>
2021-01-02 18:58:12 +0800

    重新开始维护这个项目，为自己开始准备知识体系。

--- easy-excel/src/main/java/com/sermon/easyexcel/excel/ExcelWriterFactory.java
+++ easy-excel/src/main/java/com/sermon/easyexcel/excel/ExcelWriterFactory.java
@@ -19,16 +19,16 @@ import java.util.List;
  * @Description
  * @Date 2019年7月3日17:32:07
  */
-public class ExcelWriterFactroy extends ExcelWriter {
+public class ExcelWriterFactory extends ExcelWriter {
     private OutputStream outputStream;
     private int sheetNo = 1;
 
-    public ExcelWriterFactroy(OutputStream outputStream, ExcelTypeEnum typeEnum) {
+    public ExcelWriterFactory(OutputStream outputStream, ExcelTypeEnum typeEnum) {
         super(outputStream, typeEnum);
         this.outputStream = outputStream;
     }
 
-    public ExcelWriterFactroy write(List<? extends BaseRowModel> list, String sheetName,
+    public ExcelWriterFactory write(List<? extends BaseRowModel> list, String sheetName,
                                     BaseRowModel object) {
         this.sheetNo++;
         try {
